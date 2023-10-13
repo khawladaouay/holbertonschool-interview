49979eac5a82a3b2d2c9d7946f722d5b5bef201d
Khaoula <daouaykhawla782@gmail.com>
2023-10-13 09:28:40 +0100

    Change comes from within

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,11 +3,7 @@
 
 
 def makeChange(coins, total):
-    """
-    Given a pile of coins of different values,
-    determine the fewest number of coins needed
-    to meet a given amount total
-    """
+    """Given a pile of coins of different values,determine the fewest number of coins needed to meet a given amount total"""
     if total <= 0:
         return 0
     if not coins or min(coins) > total:
